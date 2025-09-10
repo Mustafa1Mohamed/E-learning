0c83cdb4ad4125397d9e8a874d3c286a8db2a3c3
Mustafa <mm0118250@gmail.com>
2025-09-10 20:27:12 +0300

    installing tailwindcss in the project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route>
-          
-          </Route>
-      </Routes>
-      </BrowserRouter>
+    <div className='bg-black'>
+      <div className='text-3xl font-bold underline text-center text-blue-500'>
+        hello world
+      </div>
     </div>
   );
 }
