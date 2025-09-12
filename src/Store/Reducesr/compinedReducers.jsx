7d8473a72fe9fce8d9fec3566a7452f6615ab068
Mustafa <mm0118250@gmail.com>
2025-09-13 02:30:05 +0300

    removing unneccessary files

--- src/Store/Reducesr/compinedReducers.jsx
+++ src/Store/Reducesr/compinedReducers.jsx
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux';
 import FavReducers from './FavReducer'
-import moviesReducer from './GetMoviesReducer';
-
 export default combineReducers({
-    moviesReducer: moviesReducer,
     FavReducers: FavReducers
 })
