e9a83dc4a33b29ebb45d64dbe542988948f8f3ad
Mustafa <mm0118250@gmail.com>
2025-09-13 12:16:46 +0300

    just finishing whishlist and some changes in the styling on the add to whishlist button and displaying the added courses in the whishlist page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { HeartIcon as SolidHeart } from "@heroicons/react/24/solid";
 import { HeartIcon as OutlineHeart } from "@heroicons/react/24/outline";
-import { useDispatch, useSelector } from "react-redux"; 
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toggleFav } from "../Store/actions/FavAction";
+import { toggleWhishlist } from "../Store/actions/WhishListAction";
 export default function Card({
     id,
     course_name,
@@ -17,8 +18,10 @@ export default function Card({
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const favorites = useSelector((state) => state.FavReducers.favorites);
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     const isFavorite = favorites.some((course) => course.id === id);
+    const whishlist = useSelector((state) => state.WhishlistReducer.whishlist);
+    const isInWishlist = whishlist.some((course) => course.id === id);
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     const handleToggleFavorite = () => {
         if (!currentUser) {
             navigate("/login");
@@ -26,6 +29,23 @@ export default function Card({
         }
         dispatch(
             toggleFav({
+                id,
+                course_name,
+                course_plan,
+                course_image,
+                course_price,
+                course_description,
+            })
+        );
+    };
+
+    const handleToggleWishlist = () => {
+        if (!currentUser) {
+            navigate("/login");
+            return;
+        }
+        dispatch(
+            toggleWhishlist({
                 id,
                 course_name,
                 course_plan,
@@ -92,23 +112,30 @@ export default function Card({
                         </Link>
                     )}
 
-                    {currentUser?(!isFavoritesPage && (
+                    {currentUser ? (!isFavoritesPage && (
                         <button className="text-md text-center font-medium text-white bg-green-600 hover:bg-green-500 px-3 py-2 rounded-md">
                             Add to Cart
                         </button>
-                    )):(
+                    )) : (
                         <Link to="/login" className="text-md text-center font-medium text-white bg-green-600 hover:bg-green-500 px-3 py-2 rounded-md">
                             Add to Cart
                         </Link>
                     )}
-                    {currentUser?(!isFavoritesPage && (
-                    <button className="text-md text-center font-medium text-white bg-yellow-500 hover:bg-yellow-400 px-3 py-2 rounded-md">
-                        Add to Wishlist
-                    </button>)):(
+                    {currentUser ? (!isFavoritesPage && (
+                        <button
+                            className={`flex justify-center items-center gap-x-2 text-md text-center font-medium text-white bg-yellow-500 hover:bg-yellow-400 px-3 py-2 rounded-md`}
+                            onClick={handleToggleWishlist}
+                        >
+                            <span>Add to Wishlist</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill={isInWishlist ? "white" : "none"} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                            </svg>
+                        </button>
+                    )) : (
 
-                    <Link to="/login" className="text-md text-center font-medium text-white bg-yellow-500 hover:bg-yellow-400 px-3 py-2 rounded-md">
-                        Add to Wishlist
-                    </Link>
+                        <Link to="/login" className="text-md text-center font-medium text-white bg-yellow-500 hover:bg-yellow-400 px-3 py-2 rounded-md">
+                            Add to Wishlist
+                        </Link>
                     )}
                 </div>
             </div>
